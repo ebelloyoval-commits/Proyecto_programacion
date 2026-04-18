c37951a642b11db3b4495d696ddf91215792b27f
joseramon4208 <cortez.olivares.jose@gmail.com>
2026-04-17 23:24:28 -0600

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# Versión actualizada
-# Integrantes: Elsy Abril Bello Yoval
-# Descripción: Programa básico
-
-print("Sistema funcionando correctamente")
-#jose ramon cortez olivares
-# Dante Emmanuel Sanchez Hernandez
